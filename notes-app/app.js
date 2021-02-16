7924345a4be64eac5cacc24905711f40e87f9b29
jaywomack <womackjw@hotmail.com>
2021-02-15 17:00:32 -0800

    Adding a playground environment to explore json and finishing building out notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,55 @@
 const chalk = require('chalk');
-const validator = require('validator');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const logError = function (input) {
-  return console.log(chalk.red.bgBlue.bold.underline(`${input}`));
-};
+// customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note Body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
+  }
+});
+
+// create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'remove a note',
+  handler: function () {
+    console.log('Removing the note');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'list the notes',
+  handler: function () {
+    console.log('list all the notes');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'read a command',
+  handler: function () {
+    console.log('reading the command');
+  }
+});
+
+yargs.parse();
